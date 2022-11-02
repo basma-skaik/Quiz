7ff97780483948d5db8907eb7e1bb6f3f779c9c2
basma-skaik <basmahskaik@gmail.com>
2022-11-02 13:03:07 +0200

    buttonOnBoarding

--- src/screens/onBoarding/OnBoardingScreen.jsx
+++ src/screens/onBoarding/OnBoardingScreen.jsx
@@ -1,14 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View, useWindowDimensions} from 'react-native';
+import React from 'react';
+import Button from '../../shared-copmonents/button/Button';
 
 const OnBoardingScreen = () => {
+  const window = useWindowDimensions();
   return (
-    <View>
+    <View style={[styles.container, window.height]}>
       <Text>OnBoardingScreen</Text>
+      <View style={styles.btnContainer}>
+        <Button pressedScreen={''} lable={'Get started'} />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default OnBoardingScreen
+export default OnBoardingScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btnContainer: {
+    justifyContent: 'center',
+  },
+});
--- src/shared-copmonents/button/Button.jsx
+++ src/shared-copmonents/button/Button.jsx
@@ -1,20 +1,37 @@
-import {Pressable, StyleSheet, Text, View} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  useWindowDimensions,
+} from 'react-native';
 import React from 'react';
 
-const Button = ({lable}) => {
+const Button = ({lable, pressedScreen}) => {
+  const width = useWindowDimensions();
   return (
-    <Pressable>
-      <Text>{lable}</Text>
-    </Pressable>
+    // <TouchableOpacity style={[styles.btnStyle, width]}>
+    <TouchableOpacity style={styles.btnStyle}>
+      <Text style={styles.btnLabel} onPress={{pressedScreen}}>
+        {lable}
+      </Text>
+    </TouchableOpacity>
   );
 };
 
 export default Button;
 
 const styles = StyleSheet.create({
-  pressableStyle: {
-    backgroundColor: 'orange',
+  btnStyle: {
+    backgroundColor: '#6ab6d4',
     paddingHorizontal: 12,
     paddingVertical: 4,
+    borderRadius: 6,
+    width: 270,
+    height: 60,
+  },
+  btnLabel: {
+    color: '#fff',
+    fontSize: 25,
+    textAlign: 'center',
   },
 });
