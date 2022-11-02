7369138ffcab26aa2ee59250993ba100a6ac634f
Ahmed Ghanim <ahmedghanim317@gmail.com>
2022-11-02 12:17:26 +0300

    I add navigation and it's screen

---
+++ src/navigation/MainStack.jsx
@@ -0,0 +1,24 @@
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import FiltersScreen from '../screens/filters/FiltersScreen';
+import OnBoardingScreen from '../screens/onBoarding/OnBoardingScreen';
+import QuizScreen from '../screens/quiz/QuizScreen';
+import QuizResultScreen from '../screens/quizResult/QuizResultScreen';
+
+const MainStack = () => {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <Stack.Navigator initialRouteName="OnBoardingScreen"
+      screenOptions={{
+        headerShown:false
+      }}
+    >
+      <Stack.Screen name="OnBoardingScreen" component={OnBoardingScreen} />
+      <Stack.Screen name="FiltersScreen" component={FiltersScreen} />
+      <Stack.Screen name="QuizScreen" component={QuizScreen} />
+      <Stack.Screen name="QuizResultScreen" component={QuizResultScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default MainStack;
