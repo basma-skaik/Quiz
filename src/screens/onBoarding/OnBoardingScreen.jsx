7d9485438a5d2cc738a953c0dd9ff5777eba1319
Tariq Hijazi <Tariq.t.m.hijazi@gmail.com>
2022-11-05 12:49:57 +0200

    onboarding

--- src/screens/onBoarding/OnBoardingScreen.jsx
+++ src/screens/onBoarding/OnBoardingScreen.jsx
@@ -1,14 +1,56 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View, Image} from 'react-native';
+import React from 'react';
+import ListItem from './component/listItem';
+import Button from './component/Button/Button';
 
 const OnBoardingScreen = () => {
+  const listString = [
+    '• Multiple Ways Of Testing 💡',
+    '• Millions Of Questions 👀',
+    '• More And More Categories 📚',
+    '• Clean And Uniqe Style 💅',
+  ];
   return (
-    <View>
-      <Text>OnBoardingScreen</Text>
+    <View style={styles.container}>
+      <Image
+        source={require('../../../assets/img/Illustration.png')}
+        style={styles.img}
+        resizeMode={'contain'}
+      />
+      <View>
+        <Text style={styles.title}>Test Your Knowledge</Text>
+      </View>
+      <View style={styles.list}>
+        {listString.map((item, index) => (
+          <ListItem key={index} text={item} />
+        ))}
+      </View>
+      <Button buttonStyle={styles.button} title="Get Started" />
     </View>
-  )
-}
+  );
+};
 
-export default OnBoardingScreen
+export default OnBoardingScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flex: 1,
+  },
+  img: {
+    marginTop: 50,
+    height: 300,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#c6ceff',
+    marginVertical: 30,
+  },
+  list: {
+    alignSelf: 'center',
+  },
+  button: {
+    marginTop: 120,
+  },
+});
