5a229433781e2c2cb6f05e006869a3dcfb92e9df
Tariq Hijazi <Tariq.t.m.hijazi@gmail.com>
2022-11-10 14:40:13 +0200

    final

--- src/context/QuizContext.jsx
+++ src/context/QuizContext.jsx
@@ -11,10 +11,6 @@ export const AppContextProvider = ({children}) => {
     value === 0 ? setScoure(0) : setScoure(prev => prev + 1);
   };
 
-  const resetQuiz = () => {
-    setQuestions([]);
-    setScoure(0);
-  };
 
   const fetchQuestions = (
     numOfQues = 10,
